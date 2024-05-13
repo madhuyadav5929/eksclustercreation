39ae11899b2753d940393c4187aef6adbc20adcc
madhuyadv5929 <udarimadhu0@gmail.com>
2024-05-13 14:45:19 +0530

    jenkins installation

--- 02-task4/provider.tf
+++ 02-task4/provider.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.40.0"
-    }
-  }
-backend "s3" {
-  bucket         = "ekscluster5929"
-  key            = "k8s-eksctl"
-  region         = "us-east-1"
-  dynamodb_table = "aws-locking"
-}
-  
-}
-
 provider "aws" {
-  # Configuration options
+  region = "us-east-1"
 }
